95ba767f7a054dcb132af89b4afb6a481ae136f5
shayan ghazali <shayan97.tti@gmail.com>
2021-08-14 00:23:46 -0700

    to work

--- src/main/resources/static/assets/ts/hyper-admin-layout-functions.ts
+++ src/main/resources/static/assets/ts/hyper-admin-layout-functions.ts
@@ -1,8 +1,23 @@
 class LayoutFunctions {
     searchSugestion!: HTMLDivElement;
+    sidenavCollapse!: NodeListOf<HTMLDivElement>;
+    isDisabled:boolean = false;
     constructor() {
         this.searchSugestion = document.querySelector('.search-form .dropdown-menu')! as HTMLDivElement;
+        this.sidenavCollapse = document.querySelectorAll('.side-nav .accordion-collapse')! as NodeListOf<HTMLDivElement>;
         this.init();
+    }
+    private checkStorage() {
+        if (typeof (Storage) !== "undefined") {
+            localStorage.setItem("hyperSetting", "hide-side-nav");
+            if (localStorage.getItem("hyperSetting")) {
+                document.body.classList.add('hide-side-nav');
+            }
+        }
+        this.sidenavCollapse.forEach((Element) => {
+            Element.classList.add('show');
+        });
+
     }
     private init() {
         document.querySelector('.setting-btn-item .btn')!.addEventListener("click", () => {
@@ -20,6 +35,13 @@ class LayoutFunctions {
         document.querySelector('.search-form input')!.addEventListener("blur", () => {
             setTimeout(() => {
                 this.searchSugestion.classList.remove('show');
+            });
+        });
+        document.querySelectorAll('.side-nav .accordion').forEach((Element) => {
+            Element.addEventListener('hide.bs.collapse', (e) => {
+                if(isDisabled){
+                    e.preventDefault();
+                }
             });
         });
     }
