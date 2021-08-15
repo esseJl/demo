c8973107fd95a25268a30734babb1ea03364299f
unknown <shayan97.tti@gmail.com>
2021-08-15 13:04:05 +0430

    layout technically done

--- src/main/resources/static/assets/js/hyper-admin-layout-functions.js
+++ src/main/resources/static/assets/js/hyper-admin-layout-functions.js
@@ -1,9 +1,40 @@
 "use strict";
 var LayoutFunctions = /** @class */ (function () {
     function LayoutFunctions() {
+        this.isDisabled = false;
         this.searchSugestion = document.querySelector('.search-form .dropdown-menu');
+        this.sidenavCollapse = document.querySelectorAll('.side-nav .accordion-collapse');
+        this.sidenavCollapseBtn = document.querySelectorAll('.side-nav .accordion-button');
+        this.checkStorage();
         this.init();
     }
+    LayoutFunctions.prototype.checkStorage = function () {
+        if (typeof (Storage) !== "undefined" && localStorage.getItem("hyperSide")) {
+            document.body.classList.add('hide-side-nav');
+            this.accordionState(true);
+        }
+    };
+    LayoutFunctions.prototype.accordionState = function (state) {
+        if (state) {
+            this.sidenavCollapse.forEach(function (Element) {
+                Element.classList.add('show');
+            });
+            this.sidenavCollapseBtn.forEach(function (Element) {
+                Element.setAttribute('aria-expanded', 'true');
+            });
+            this.isDisabled = true;
+        }
+        else {
+            this.sidenavCollapse.forEach(function (Element) {
+                Element.classList.remove('show');
+            });
+            this.sidenavCollapseBtn.forEach(function (Element) {
+                Element.setAttribute('aria-expanded', 'false');
+                Element.classList.add('collapsed');
+            });
+            this.isDisabled = false;
+        }
+    };
     LayoutFunctions.prototype.init = function () {
         var _this = this;
         document.querySelector('.setting-btn-item .btn').addEventListener("click", function () {
@@ -11,6 +42,14 @@ var LayoutFunctions = /** @class */ (function () {
         });
         document.querySelector('.hamburger-btn').addEventListener("click", function () {
             document.body.classList.toggle('hide-side-nav');
+            if (document.body.classList.contains('hide-side-nav')) {
+                localStorage.setItem("hyperSide", "active");
+                _this.accordionState(true);
+            }
+            else {
+                localStorage.removeItem("hyperSide");
+                _this.accordionState(false);
+            }
         });
         document.querySelector('.search-form input').addEventListener("focus", function () {
             _this.searchSugestion.classList.add('show');
@@ -21,6 +60,13 @@ var LayoutFunctions = /** @class */ (function () {
         document.querySelector('.search-form input').addEventListener("blur", function () {
             setTimeout(function () {
                 _this.searchSugestion.classList.remove('show');
+            }, 100);
+        });
+        document.querySelectorAll('.side-nav .accordion').forEach(function (Element) {
+            Element.addEventListener('hide.bs.collapse', function (e) {
+                if (_this.isDisabled) {
+                    e.preventDefault();
+                }
             });
         });
     };
--- src/main/resources/static/assets/ts/hyper-admin-layout-functions.ts
+++ src/main/resources/static/assets/ts/hyper-admin-layout-functions.ts
@@ -1,30 +1,59 @@
 class LayoutFunctions {
+
     searchSugestion!: HTMLDivElement;
     sidenavCollapse!: NodeListOf<HTMLDivElement>;
-    isDisabled:boolean = false;
+    sidenavCollapseBtn!: NodeListOf<HTMLDivElement>;
+    isDisabled: boolean = false;
+
     constructor() {
         this.searchSugestion = document.querySelector('.search-form .dropdown-menu')! as HTMLDivElement;
         this.sidenavCollapse = document.querySelectorAll('.side-nav .accordion-collapse')! as NodeListOf<HTMLDivElement>;
+        this.sidenavCollapseBtn = document.querySelectorAll('.side-nav .accordion-button')! as NodeListOf<HTMLDivElement>;
+        this.checkStorage();
         this.init();
     }
-    private checkStorage() {
-        if (typeof (Storage) !== "undefined") {
-            localStorage.setItem("hyperSetting", "hide-side-nav");
-            if (localStorage.getItem("hyperSetting")) {
-                document.body.classList.add('hide-side-nav');
-            }
-        }
-        this.sidenavCollapse.forEach((Element) => {
-            Element.classList.add('show');
-        });
 
+    private checkStorage() {
+        if (typeof (Storage) !== "undefined" && localStorage.getItem("hyperSide")) {
+            document.body.classList.add('hide-side-nav');
+            this.accordionState(true);
+        }
     }
+
+    private accordionState(state: boolean) {
+        if (state) {
+            this.sidenavCollapse.forEach((Element) => {
+                Element.classList.add('show');
+            });
+            this.sidenavCollapseBtn.forEach((Element) => {
+                Element.setAttribute('aria-expanded' , 'true');
+            });
+            this.isDisabled = true;
+        } else {
+            this.sidenavCollapse.forEach((Element) => {
+                Element.classList.remove('show');
+            });
+            this.sidenavCollapseBtn.forEach((Element) => {
+                Element.setAttribute('aria-expanded' , 'false');
+                Element.classList.add('collapsed');
+            });
+            this.isDisabled = false;
+        }
+    }
+
     private init() {
         document.querySelector('.setting-btn-item .btn')!.addEventListener("click", () => {
             document.body.classList.toggle('show-setting-bar');
         });
         document.querySelector('.hamburger-btn')!.addEventListener("click", () => {
             document.body.classList.toggle('hide-side-nav');
+            if (document.body.classList.contains('hide-side-nav')) {
+                localStorage.setItem("hyperSide" , "active");
+                this.accordionState(true);
+            } else {
+                localStorage.removeItem("hyperSide");
+                this.accordionState(false);
+            }
         });
         document.querySelector('.search-form input')!.addEventListener("focus", () => {
             this.searchSugestion.classList.add('show');
@@ -35,15 +64,17 @@ class LayoutFunctions {
         document.querySelector('.search-form input')!.addEventListener("blur", () => {
             setTimeout(() => {
                 this.searchSugestion.classList.remove('show');
-            });
+            }, 100);
         });
         document.querySelectorAll('.side-nav .accordion').forEach((Element) => {
             Element.addEventListener('hide.bs.collapse', (e) => {
-                if(isDisabled){
+                if (this.isDisabled) {
                     e.preventDefault();
                 }
             });
         });
     }
+
 }
+
 new LayoutFunctions();
